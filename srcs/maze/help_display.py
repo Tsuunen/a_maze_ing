e749ca2c7ae1f68f465b3fc2ac5a20e2b80e3899
Remi Laforgue <relaforg@z4r2p4.42lyon.fr>
2026-01-19 14:42:47 +0100

    Refactor all maze display

---
+++ srcs/maze/help_display.py
@@ -0,0 +1,97 @@
+from .button import Button
+from time import sleep
+
+
+class HelpDisplay:
+    def __init__(self, m, mlx, display) -> None:
+        PAD_X = 30
+        GAP_Y = 20
+        self.display = display
+        self.m = m
+        self.mlx = mlx
+        self.raw_buts = [
+            {
+                "label": "r: Generate a new maze",
+                "action": lambda: display.regen_maze(display.config)
+            },
+            {
+                "label": "t: Generate a new random maze",
+                "action": lambda: display.regen_maze(
+                    display.gen_random_config())
+            },
+            {
+                "label": "x: export current maze config",
+                "action": display.export_config
+            },
+            {
+                "label": "p: Toggle path",
+                "action": display.toggle_path
+            },
+            {
+                "label": "w: Change wall color",
+                "action": display.change_wall_color
+            },
+            {
+                "label": "l: Change 42 color",
+                "action": display.change_logo_color
+            },
+            {
+                "label": "q: Quit",
+                "action": lambda: display.m.mlx_loop_exit(self.mlx)
+            }
+        ]
+        self.buttons = []
+        tmp = GAP_Y
+        for button in self.raw_buts:
+            but = Button(PAD_X, tmp + GAP_Y,
+                         button["action"], button["label"])
+            tmp += but.h + GAP_Y
+            self.buttons.append(but)
+        self.win = self.m.mlx_new_window(self.mlx,
+                                         max(b.w for b in self.buttons) +
+                                         PAD_X * 2, GAP_Y + len(self.buttons) *
+                                         20 + (len(self.buttons) - 1) *
+                                         GAP_Y + GAP_Y * 2,
+                                         "Help - relaforg & nahecre")
+
+    def run(self):
+        self.m.mlx_hook(self.win, 33, 0,
+                        lambda _: self.m.mlx_loop_exit(self.mlx), None)
+        self.m.mlx_mouse_hook(self.win, self.on_mouse, None)
+        self.m.mlx_key_hook(self.win, self.key_pressed, None)
+        self.m.mlx_string_put(self.mlx, self.win, 10, 10,
+                              0xFFFFFFFF, "Help window")
+        for i in self.buttons:
+            sleep(0.001)
+            self.m.mlx_string_put(self.mlx, self.win, i.x,
+                                  i.y, 0xFFFFFFFF, i.label)
+
+    def on_mouse(self, button, x, y, _):
+        if (button == 1):
+            for i in self.buttons:
+                if (i.contains(x, y)):
+                    i.action()
+                    return
+
+    def key_pressed(self, keycode: int, _):
+        if (keycode == 113):  # 'q'
+            self.m.mlx_loop_exit(self.mlx)
+        elif (keycode == 112):  # 'p'
+            self.display.toggle_path()
+        elif (keycode == 114):  # 'r'
+            self.display.regen_maze(self.display.config)
+        elif (keycode == 119):  # 'w'
+            self.display.change_wall_color()
+        elif (keycode == 108):  # 'l'
+            self.display.change_logo_color()
+        elif (keycode == 116):  # 't'
+            self.display.regen_maze(self.display.gen_random_config())
+        elif (keycode == 120):  # 'x'
+            self.display.export_config()
+        elif (keycode == 61):  # '='
+            self.display.ratio += 1/10
+            self.display.recreate_win()
+        elif (keycode == 45):  # '-'
+            if (self.display.ratio > 0.4):
+                self.display.ratio -= 1/10
+                self.display.recreate_win()
