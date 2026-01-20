ab83da431a721cf9c618755bba2e799bb2268d25
Nathan Hecre <nahecre@z4r3p5.42lyon.fr>
2026-01-20 14:47:08 +0100

    fused maze_gen and solver

--- srcs/generator/maze_gen.py
+++ srcs/generator/maze_gen.py
@@ -5,6 +5,11 @@ from ..maze.parser import Maze
 
 
 class MazeGen:
+    """
+    The MazeGen obj aims to generate mazes using the dfs algorithm
+    The int func takes a Config Obj and can export the Maze as a file or
+    a Maze obj
+    """
     def __init__(self, conf: Config):
         self.width = conf.width
         self.height = conf.height
@@ -160,7 +165,6 @@ exit has been moved")
             self.project_out()
 
         if self.shape == "diamond":
-            from .solver import AStar
             center = (size // 2, size // 2)
             for line in range(size):
                 for col in range(size):
@@ -280,7 +284,6 @@ exit has been moved")
         solve the maze
         return a string representing mooves necessary to solve it
         """
-        from .solver import AStar
         solver: AStar = AStar(self)
         solution: str | None = solver.solve(self)
         if not solution:
@@ -387,3 +390,126 @@ exit has been moved")
             if neighbors[0] == 3:
                 pos.append((pos[-1][0] - 1, pos[-1][1]))
         self.remove_square_holes()
+
+
+class AStar:
+    """
+    AStar is a class that aims to solve mazes generated with the MazeGen obj
+    it must be instanciate before use
+    init only takes a maze as a parameter
+    """
+    def __init__(self, maze: MazeGen):
+        self.open = [maze.entry]
+        self.closed: set[tuple[int, int]] = set()
+        self.came_from: dict[tuple[int, int], tuple[int, int]] = dict()
+        self.g = {maze.entry: 0}
+        self.f = {maze.entry: self.dist(maze.entry, maze.exit)}
+
+    @staticmethod
+    def dist(src: tuple[int, int], dest: tuple[int, int]) -> int:
+        """
+        calculates the manathan distance between two nodes
+        """
+        return abs(src[0] - dest[0]) + abs(src[1] - dest[1])
+
+    @staticmethod
+    def neighbors(maze: MazeGen, pos: tuple[int, int]) \
+            -> list[tuple[int, int]]:
+        """
+        return the coordinate of the avaible neighbors from 'pos' cell
+        """
+        neighbors_coords: list[tuple[int, int]] = []
+        walls: int = maze.lst_repr[pos[1]][pos[0]]
+
+        if not walls & 1:
+            neighbors_coords.append((pos[0], pos[1] - 1))
+        if not walls & 2:
+            neighbors_coords.append((pos[0] + 1, pos[1]))
+        if not walls & 4:
+            neighbors_coords.append((pos[0], pos[1] + 1))
+        if not walls & 8:
+            neighbors_coords.append((pos[0] - 1, pos[1]))
+        return neighbors_coords
+
+    def best_node(self) -> tuple[int, int]:
+        """
+        finds the node with the lowest f value
+        """
+        best_node = self.open[0]
+        for node in self.open[1:]:
+            if self.f[best_node] > self.f[node]:
+                best_node = node
+        return best_node
+
+    def close_node(self, current: tuple[int, int]) -> None:
+        """
+        shift my current node from self.open to self.closed
+        """
+        self.open.remove(current)
+        self.closed.add(current)
+
+    def compare_paths(self, current: tuple[int, int], next: tuple[int, int]) \
+            -> bool:
+        """
+        takes in a node and where we are trying to establish a path from
+        and return true if the path should be established
+        (true if no path to this cell exists or if it exists and is longer)
+        """
+        if next not in self.open and next not in self.g:
+            return True
+        if self.g[next] > self.g[current] + 1:
+            return True
+        return False
+
+    def solve_paths(self, maze: MazeGen) -> bool:
+        """
+        finds a path from entry to exit
+        puts every explored path in self.came_from
+        """
+        current: tuple[int, int]
+        while self.open:
+            current = self.best_node()
+            self.close_node(current)
+            if current == maze.exit:
+                return True
+            neighbors = self.neighbors(maze, current)
+            for node in neighbors:
+                if node in self.closed:
+                    continue
+                if self.compare_paths(current, node):
+                    if node not in self.open:
+                        self.open.append(node)
+                    self.g[node] = self.g[current] + 1
+                    self.f[node] = self.g[node] + \
+                        self.dist(node, maze.exit)
+                    self.came_from[node] = current
+        return False
+
+    def find_path(self, maze: MazeGen) -> str:
+        """
+        parse the came_from dict to find the best path from entry to exit
+        """
+        output: str = ""
+        current: tuple[int, int] = maze.exit
+        while current in self.came_from:
+            if self.came_from[current][0] > current[0]:
+                output += "W"
+            elif self.came_from[current][0] < current[0]:
+                output += "E"
+            elif self.came_from[current][1] > current[1]:
+                output += "N"
+            elif self.came_from[current][1] < current[1]:
+                output += "S"
+            current = self.came_from[current]
+        return output[::-1]
+
+    def solve(self, maze: MazeGen) -> str | None:
+        """
+        solves a given maze using the A* algorithm
+        output the solution from entry in a string formated with maze notation
+        (N, S, E, W)
+        """
+        if self.solve_paths(maze):
+            return self.find_path(maze)
+        else:
+            return None
